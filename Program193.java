46e08f113ad59d5ba84d4fbe273d198863cfc26d
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-06-06 02:09:08 +0530

    Print Hello world

---
+++ Program193.java
@@ -0,0 +1,16 @@
+/////////////////////////////////////////////////////////
+// Function Name : main()
+// Description   : Print Hello world
+// Input         : -------
+// Output        : Hello world
+// Author        : Shivaji Das
+// Date          : 6 June 2021
+///////////////////////////////////////////////////////
+
+class Program193
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Hello world");
+	}
+}

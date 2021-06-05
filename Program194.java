c29760899b97237509c8c0de99727ed6748ff808
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-06-06 02:12:24 +0530

    Addition of 2 number in main function

---
+++ Program194.java
@@ -0,0 +1,19 @@
+/////////////////////////////////////////////////////////
+// Function Name : main()
+// Description   : Addition of 2 number in main function
+// Input         : -------
+// Output        : 30
+// Author        : Shivaji Das
+// Date          : 6 June 2021
+///////////////////////////////////////////////////////
+
+class Program194
+{
+	public static void main(String arg[])
+	{
+		int ans = 0;
+		int no1 = 10, no2 = 20;
+		ans = no1 + no2;
+		System.out.println("Addition is : "+ans);
+	}
+}

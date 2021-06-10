838bc7903c87d403af66b777e7817432d7f1033d
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-06-10 22:28:15 +0530

    Accept and Display normal matrix(using object oriented Approach)

---
+++ Program215.java
@@ -0,0 +1,97 @@
+///////////////////////////////////////////////////////////
+// Function Name : -------
+// Description   : Accept and Display normal matrix(using object oriented Approach)
+// Input         : Row      :2
+//               : Colum    :2
+//               : Elements :1 2 3 4 
+// Output        : 1   2
+//               : 3   4
+// Author        : Shivaji Das
+// Date          : 10 June 2021
+//////////////////////////////////////////////////////////
+
+
+import java.util.*;
+
+class Matrix
+{
+	public int Arr[][];
+	public Matrix(int iRow, int iCol)
+	{
+		Arr = new int[iRow][iCol];
+	}
+	protected void finalize()
+	{
+		Arr = null;
+	}
+	public void Accept()
+	{
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Please enter th elements");
+		for(int i = 0; i < Arr.length; i++)	// Rows
+		{
+			for(int j = 0; j < Arr[i].length; j++)	// Columns
+			{
+				Arr[i][j] = sobj.nextInt();
+			}
+		}
+	}
+	public void Display()
+	{
+		System.out.println("Elemenets of Matrix are : ");
+		for(int i = 0; i < Arr.length; i++)	// Rows
+		{
+			for(int j = 0; j < Arr[i].length; j++)	// Columns
+			{
+				System.out.print(Arr[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+}
+
+class Program215
+{
+	public static void main(String str[])	
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter number of rows");
+		int row = sobj.nextInt();
+		
+		System.out.println("Enter number of columns");
+		int col = sobj.nextInt();
+
+		Matrix mobj = new Matrix(row,col);
+		mobj.Accept();
+		mobj.Display();
+
+		System.gc();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

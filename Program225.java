149393c8587f3864cebf3cd9dbb0181df9e3eaa1
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-06-14 00:12:30 +0530

    To Find Length of String and trim the String usng trim and replace all function

---
+++ Program225.java
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////
+// Function Name : main()
+// Description   : To Find Length of String and trim the String usng trim and replace all function
+// Input         : String
+// Output        : String,Integer
+// Author        : Shivaji Das
+// Date          : 14 June 2021
+//////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class Program225
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter string");
+		String str = sobj.nextLine();
+		System.out.println("String length is : "+str.length());
+		String temp = str.trim();
+		System.out.println("New string is : "+temp);
+		System.out.println("Length of new string is : "+temp.length());
+		String temp2 = temp.replaceAll("\\s","");
+		System.out.println("Modified string is : "+temp2);
+	}
+}
+
+
+
+
